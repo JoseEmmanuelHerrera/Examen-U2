0301f0aed9e2e4fed279f9fd53c45ef196a21174
JoseEmmanuelHerrera <20193tn048@utez.edu.mx>
2021-06-25 15:52:12 -0500

    Ayuda

---
+++ js/js.js
@@ -0,0 +1,5 @@
+const Busca = () => {
+    var busca;
+    busca = document.getElementById("busca").value;
+    alert(`La busqueda con nombre ${busca} no se encuentra, por lo cual te solicitamos intentar más tarde`)
+}
